017cb26a21b9269e1efe1a97f1c0cfd487464f39
Gabriel Sousa <gds8@aluno.ifnmg.edu.br>
2024-10-16 15:35:19 -0300

    Add files via upload

---
+++ Models/Reserva.cs
@@ -0,0 +1,62 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // Console.WriteLine($"{Suite.Capacidade} - {hospedes.Count}");
+            if (Suite.Capacidade<hospedes.Count)
+            {
+                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("A quantidade de hospedes não pode exceder a capacidade da suite!");
+                
+            }
+            else
+            {
+               Hospedes = hospedes;
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            int quantidade = Hospedes.Count;
+            return quantidade;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            
+            decimal valor = 0;
+            valor = DiasReservados*Suite.ValorDiaria;
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+          
+            if (DiasReservados>=10)
+            {
+               decimal desconto = valor * 0.1M;
+               valor = valor - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
